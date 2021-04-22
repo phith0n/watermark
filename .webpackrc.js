5b116b88a5973fc0a540a148dfbaf160c7a42160
phith0n <phith0n.ph2f@gmail.com>
2021-04-22 22:30:06 +0800

    move output directory

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 export default {
   env: {
     development: {
       extraBabelPlugins: ["dva-hmr"]
     }
   },
+  "outputPath": path.resolve(__dirname, 'docs'),
 };
