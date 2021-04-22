79560b79968db9ca5c53e9f9b2a8abbc7f32ecb1
phith0n <phith0n.ph2f@gmail.com>
2021-04-22 22:23:00 +0800

    build

--- src/routes/Donation/Donation.js
+++ src/routes/Donation/Donation.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Block from "dxc-flex";
-import wx from "./wx.jpeg";
-import zfb from "./zfb.jpg";
 import DonationItem from "./DonationItem";
 
 export default class Donation extends React.Component {
@@ -9,11 +7,7 @@ export default class Donation extends React.Component {
     return (
       <Block layout="vertical" horizontal="center" style={{ margin: "30px 0" }}>
         <Block horizontal="center" style={{ fontSize: 18, fontWeight: 700, marginBottom: 15 }}>
-          捐助作者
-        </Block>
-        <Block>
-          <DonationItem name="微信" qr={wx} />
-          <DonationItem name="支付宝" qr={zfb} />
+          感谢支持
         </Block>
       </Block>
     );
